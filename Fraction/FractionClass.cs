2d5a5d2b95a6538e82eb47e5005ad984127eee53
Gh0sTed <113895444+ProfessorMB21@users.noreply.github.com>
2025-01-17 15:52:39 +0300

    Add project files.

---
+++ Fraction/FractionClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    public class FractionClass
+    {
+        private int _numerator;
+        private int _denominator;
+        private int _wholeNum = 0;
+        public int Numerator => _numerator;
+        public int Denominator => _denominator;
+
+        public FractionClass(int a, int b) 
+        {
+            _numerator = a;
+            _denominator = b;
+        }
+        public FractionClass(int numerator, int denominator, int wholeNum)
+        {
+            _numerator = numerator;
+            _denominator = denominator;
+            _wholeNum = wholeNum;
+        }
+        public FractionClass() { }
+        private FractionClass AddWithSameDenominator(FractionClass other)
+        {
+            var numerator = _numerator + other._numerator;
+            return new FractionClass(numerator, _denominator);
+        }
+        private int gcd(int a, int b)
+        {
+            if (a == 0) return b;
+            return gcd(b % a, a);
+        }
+        private int lcm(int a, int b) => (a * b) / gcd(a, b);
+        private FractionClass AddWithDiffDenominator(FractionClass other)
+        {
+            int lcd = lcm(this.Denominator, other.Denominator);
+
+            _numerator *= (lcd / this.Denominator);
+            other._numerator *= (lcd / other.Denominator);
+
+            int result_num = _numerator + other._numerator;
+            return new FractionClass(result_num, lcd);
+        }
+        public FractionClass Add(FractionClass other)
+        {
+            if (Denominator == other.Denominator)
+                return AddWithSameDenominator(other);
+            return AddWithDiffDenominator(other);
+        }
+        
+        public override string ToString()
+        {
+            if (Numerator < 0 && Denominator < 0)
+                return $"{Math.Abs(_numerator)}/{Math.Abs(_denominator)}";
+            else if (Numerator > 0 && Denominator < 0 || Numerator < 0 && Denominator > 0)
+                return $"-{Math.Abs(_numerator)}/{Math.Abs(_denominator)}";
+            return $"{_numerator}/{_denominator}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is FractionClass other && Numerator == other.Numerator
+                && Denominator == other.Denominator)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ FractionTest/FractionTests.cs
@@ -0,0 +1,81 @@
+﻿using Fraction;
+
+namespace FractionTest
+{
+    public class FractionTests
+    {
+        [Fact(DisplayName = "AddWithSameDenominator with positive fractions method unit test.")]
+        public void PrivateAddWithSameDenominator_PositiveFractions_ReturnsExpectedResult()
+        {
+            var f1 = new FractionClass(1, 2);
+            var f2 = new FractionClass(3, 2);
+
+            FractionClass expectedResult = new(4, 2);
+            FractionClass actualResult = f1.Add(f2);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact(DisplayName = "AddWithDiffDenominator with positive fractions method unit test.")]
+        public void PrivateAddWithDiffDenominator_PositiveFractions_ReturnsExpected()
+        {
+            var f1 = new FractionClass(1, 2);
+            var f2 = new FractionClass(3, 5);
+
+            FractionClass expectedResult = new(11, 10);
+            FractionClass actualResult = f1.Add(f2);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory(DisplayName = "ToString method unit test.")]
+        [InlineData(1, 2, "1/2")]
+        [InlineData(3, 5, "3/5")]
+        [InlineData(0, 1, "0/1")]
+        [InlineData(6, 7, "6/7")]
+        [InlineData(-1, 9, "-1/9")]
+        [InlineData(2, -11, "-2/11")]
+        [InlineData(-1, -2, "1/2")]
+        public void ToString_ReturnsString(int numerator, int denominator, string expected)
+        {
+            var f1 = new FractionClass(numerator, denominator);
+            Assert.Equal(expected, f1.ToString());
+        }
+
+        public static IEnumerable<object[]> Fractions
+        {
+            get
+            {
+                using (var sr = new StreamReader("fraction_test.txt", System.Text.Encoding.UTF8))
+                {
+                    while (true)
+                    {
+                        var data = sr.ReadLine()?.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                        if (data is not null)
+                        {
+                            FractionClass f;
+                            try
+                            {
+                                f = new FractionClass(int.Parse(data[0]), int.Parse(data[1]));
+                            }
+                            catch
+                            {
+                                f = new FractionClass();
+                            }
+                            yield return new object[] { f, data[2] };
+                        }
+                        else yield break;
+                    };
+                }
+            }
+
+        }
+
+        [Theory(DisplayName ="ToString unit test with external source test cases.")]
+        [MemberData(nameof(Fractions))]
+        public void ToString_Fractions_ReturnsString(FractionClass f, string expected)
+        {
+            Assert.Equal(expected, f.ToString());
+        }
+    }
+}
