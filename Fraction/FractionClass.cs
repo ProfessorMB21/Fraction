c27e6cdc4660fbd8c28924d0fa5ccb40534c4610
Gh0sTed <113895444+ProfessorMB21@users.noreply.github.com>
2025-01-19 20:05:36 +0300

    implemented operators and tests; minor changes made

--- Fraction/FractionClass.cs
+++ Fraction/FractionClass.cs
@@ -7,42 +7,48 @@ using System.Threading.Tasks;
 
 namespace Fraction
 {
-    public class FractionClass
+    public class FractionClass : IComparable<FractionClass>
     {
         private int _numerator;
         private int _denominator;
-        private int _wholeNum = 0;
-        public int Numerator => _numerator;
-        public int Denominator => _denominator;
+        public int Numerator
+        {
+            get => _numerator;
+            private set => _numerator = value;
+        }
+        public int Denominator
+        {
+            get => _denominator;
+            private set
+            {
+                if (value == 0)
+                    throw new ArgumentException("Denominator cannot be zero.");
+                _denominator = value;
+            }
+        }
+        public double Value => (double)Numerator / Denominator;
 
         public FractionClass(int numerator, int denominator) 
         {
-            if (denominator == 0) 
-                throw new ArgumentException($"{nameof(denominator)} cannot be zero.");
-            _numerator = numerator;
-            _denominator = denominator;
+            Denominator = denominator;
+            Numerator = numerator;
         }
-        public FractionClass(int numerator, int denominator, int wholeNum)
+        public FractionClass(int numerator, int denominator, int wholeNumber)
+            : this(wholeNumber * denominator + numerator, denominator)
         {
-            if (denominator == 0)
-                throw new ArgumentException($"{nameof(denominator)} cannot be zero.");
-            _numerator = numerator;
-            _denominator = denominator;
-            _wholeNum = wholeNum;
+            if (wholeNumber < 0 && numerator > 0)
+                throw new ArgumentException($"In a mixed fraction, the {nameof(numerator)} must have the same sign as the whole number.");
         }
-        public FractionClass() { }
-        private FractionClass AddWithSameDenominator(FractionClass other)
-        {
-            var numerator = _numerator + other._numerator;
-            return new FractionClass(numerator, _denominator);
-        }
+        public FractionClass() : this(0, 1) { }
+        
         private int gcd(int a, int b)
         {
             if (a == 0) return b;
             return gcd(b % a, a);
         }
         private int lcm(int a, int b) => (a * b) / gcd(a, b);
-        private FractionClass AddWithDiffDenominator(FractionClass other)
+       
+        private FractionClass Add(FractionClass other)
         {
             int lcd = lcm(this.Denominator, other.Denominator);
 
@@ -52,11 +58,15 @@ namespace Fraction
             int result_num = _numerator + other._numerator;
             return new FractionClass(result_num, lcd);
         }
-        public FractionClass Add(FractionClass other)
+        private FractionClass Subtract(FractionClass other)
         {
-            if (Denominator == other.Denominator)
-                return AddWithSameDenominator(other);
-            return AddWithDiffDenominator(other);
+            int lcd = lcm(this.Denominator, other.Denominator);
+
+            _numerator *= (lcd / this.Denominator);
+            other._numerator *= (lcd / other.Denominator);
+
+            int result_num = _numerator - other._numerator;
+            return new FractionClass(result_num, lcd);
         }
         
         public override string ToString()
@@ -75,8 +85,58 @@ namespace Fraction
                 return true;
             return false;
         }
+        public override int GetHashCode()
+        {
+            return (Numerator,  Denominator).GetHashCode();
+        }
+
+        public int CompareTo(FractionClass? other)
+        {
+            if (other == null) return -1;
+            return Numerator.CompareTo(other.Numerator) & Denominator.CompareTo(other.Denominator);
+            //return Value.CompareTo(other.Value);
+        }
 
         // operators
-        
+        public static FractionClass operator+(FractionClass _this, FractionClass other)
+        {
+            if (_this is null || other is null)
+                throw new ArgumentNullException($"{(_this is null ? nameof(_this) : nameof(other))} is null.");
+            return _this.Add(other);
+        }
+        public static FractionClass operator-(FractionClass _this, FractionClass other)
+        {
+            if (_this is null || other is null)
+                throw new ArgumentNullException($"{(_this is null ? nameof(_this) : nameof(other))} is null.");
+            return _this.Subtract(other);
+        }
+        public static FractionClass operator*(FractionClass _this, FractionClass other)
+        {
+            if (other.Denominator == 0 || _this.Denominator == 0)
+                throw new DivideByZeroException("Division by zero not allowed.");
+            return new FractionClass(
+                _this.Numerator * other.Numerator,
+                _this.Denominator* other.Denominator
+                );
+        }
+        public static FractionClass operator /(FractionClass _this, FractionClass other)
+        {
+            if (other.Denominator == 0 || _this.Denominator == 0)
+                throw new DivideByZeroException("Division by zero not allowed.");
+            return new FractionClass(
+                _this.Numerator * other.Denominator,
+                _this.Denominator * other.Numerator
+                );
+        }
+        public static FractionClass operator^(FractionClass _this, int exponent)
+        {
+            var absExponent = Math.Abs(exponent);
+            var denominator = (int)Math.Pow(_this.Denominator, absExponent);
+            var numerator = (int)Math.Pow(_this.Numerator, absExponent);
+
+            if (exponent >= 0)
+                return new FractionClass(numerator, denominator);
+            return new FractionClass(denominator, numerator);
+        }
     }
 }
--- FractionTest/FractionTests.cs
+++ FractionTest/FractionTests.cs
@@ -1,34 +1,11 @@
 ﻿using Fraction;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 
 namespace FractionTest
 {
     public class FractionTests
     {
-        [Fact(DisplayName = "AddWithSameDenominator with positive fractions method unit test.")]
-        public void PrivateAddWithSameDenominator_PositiveFractions_ReturnsExpectedResult()
-        {
-            var f1 = new FractionClass(1, 2);
-            var f2 = new FractionClass(3, 2);
-
-            FractionClass expectedResult = new(4, 2);
-            FractionClass actualResult = f1.Add(f2);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Fact(DisplayName = "AddWithDiffDenominator with positive fractions method unit test.")]
-        public void PrivateAddWithDiffDenominator_PositiveFractions_ReturnsExpected()
-        {
-            var f1 = new FractionClass(1, 2);
-            var f2 = new FractionClass(3, 5);
-
-            FractionClass expectedResult = new(11, 10);
-            FractionClass actualResult = f1.Add(f2);
-
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory(DisplayName = "ToString method unit test.")]
+        [Theory(DisplayName = "ToString method unit tests.")]
         [InlineData(1, 2, "1/2")]
         [InlineData(3, 5, "3/5")]
         [InlineData(0, 1, "0/1")]
@@ -82,5 +59,184 @@ namespace FractionTest
         {
             Assert.Equal(expected, f.ToString());
         }
+
+        [Fact(DisplayName = "FractionClass constructor with two parameters only unit tests.")]
+        public void FractionClass_Constructor_Throws()
+        {
+            //FractionClass fraction = new(2, 0);
+
+            Assert.Throws<ArgumentException>(() => new FractionClass(2, 0));
+            Assert.Throws<ArgumentException>(() => new FractionClass(-4, 0));
+            Assert.Throws<ArgumentException>(() => new FractionClass(0, 0));
+        }
+
+        [Fact(DisplayName ="Raising a fraction to a positive nth power using Math.Pow method unit tests.")]
+        public void RaiseToPowerOperator_PositivePowers_FractionClass_ReturnsExpected()
+        {
+            var f = new FractionClass(1, 5);
+
+            var expected = "1/25";
+            var actual = f ^ 2;
+            Assert.Equal(expected, actual.ToString());
+
+            var f2 = new FractionClass(3, -4);
+            var expected1 = "9/16";
+            var actual1 = f2 ^ 2;
+            Assert.Equal(expected1, actual1.ToString());
+
+            var expected2 = "1/1";
+            var actual2 = f2 ^ 0;
+            Assert.Equal(expected2, actual2.ToString());
+        }
+
+        [Fact(DisplayName ="Raising a fraction to a negative nth power unit tests.")]
+        public void RaiseToPowerOperator_NegativePowers_FractionClass_ReturnsExpected()
+        {
+            var f1 = new FractionClass(2, 11);
+
+            var expected1 = "11/2";
+            var actual1 = f1 ^ -1;
+            Assert.Equal(expected1, actual1.ToString());
+
+            var expected2 = "1331/8";
+            var actual2 = f1 ^ (-3);
+            Assert.Equal(expected2, actual2.ToString());
+        }
+
+        [Fact(DisplayName ="Property Denominator unit tests.")]
+        public void PropertyDenominator_Fraction_Throws()
+        {
+            Assert.Throws<ArgumentException>(()  => new FractionClass(1, 0));
+            Assert.Throws<ArgumentException>(() => new FractionClass(-2, 0));
+            Assert.Throws<ArgumentException>(() => new FractionClass(10000, 0));
+        }
+
+        public static IEnumerable<object[]> AddOperatorTests
+        {
+            get
+            {
+                string tests_filename = "operators_test.txt";
+                using (var sr = new StreamReader(tests_filename, System.Text.Encoding.UTF8))
+                {
+                    while (true)
+                    {
+                        var data = sr.ReadLine()?.Split(',', ' ',
+                            StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+                            );
+
+                        if (data is not null)
+                        {
+                            FractionClass f1, f2;
+                            var val1 = data[0].Split('/');
+                            var val2 = data[1].Split('/');
+                            try
+                            {
+                                f1 = new FractionClass(int.Parse(val1[0]), int.Parse(val1[1]));
+                                f2 = new FractionClass(int.Parse(val2[0]), int.Parse(val2[1]));
+                            }
+                            catch
+                            {
+                                f1 = new FractionClass();
+                                f2 = new FractionClass();
+                            }
+                            yield return new object[] { f1, f2, data[2] };
+                        }
+                        else yield break;
+
+                    }
+                }
+            }
+        }
+        [Theory(DisplayName = "Addition operator on fractions unit tests.")]
+        [MemberData(nameof(AddOperatorTests))]
+        public void AddOperator_Fraction_ReturnsExpected(FractionClass f1, FractionClass f2, string expected)
+        {
+            var actual = f1 + f2;
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        public static IEnumerable<object[]> SubtractOperatorTests
+        {
+            get
+            {
+                string tests_filename = "operators_test.txt";
+                using (var sr = new StreamReader(tests_filename, System.Text.Encoding.UTF8))
+                {
+                    while (true)
+                    {
+                        var data = sr.ReadLine()?.Split(',', ' ',
+                            StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+                            );
+
+                        if (data is not null)
+                        {
+                            FractionClass f1, f2;
+                            var val1 = data[0].Split('/');
+                            var val2 = data[1].Split('/');
+                            try
+                            {
+                                f1 = new FractionClass(int.Parse(val1[0]), int.Parse(val1[1]));
+                                f2 = new FractionClass(int.Parse(val2[0]), int.Parse(val2[1]));
+                            }
+                            catch
+                            {
+                                f1 = new FractionClass();
+                                f2 = new FractionClass();
+                            }
+                            yield return new object[] { f1, f2, data[3] };
+                        }
+                        else yield break;
+
+                    }
+                }
+            }
+        }
+        [Theory(DisplayName ="Subtraction operator on fractions unit tests.")]
+        [MemberData(nameof(SubtractOperatorTests))]
+        public void SubtractOperator_Fraction_ReturnsExpected(FractionClass f1, FractionClass f2, string expected)
+        {
+            var actual = f1 - f2;
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        [Theory(DisplayName ="Multiplication operator on fractions unit tests.")]
+        [InlineData(1, 5, 3, 7, "3/35")]
+        [InlineData(4, 3, 2, 9, "8/27")]
+        [InlineData(0, 2, -4, 9, "0/18")]
+        [InlineData(-2, 5, 3, 7, "-6/35")]
+        [InlineData(2, -5, 3, 7, "-6/35")]
+        [InlineData(2, 5, -3, 7, "-6/35")]
+        [InlineData(2, 5, 3, -7, "-6/35")]
+        [InlineData(3, 4, 1, 2, "3/8")]
+        [InlineData(3, 0, -5, 8, "-15/0", Skip ="Division by zero.")]
+        [InlineData(-1, -2, -2, -3, "2/6")]
+        public void MultiplyOperator_Fraction_ReturnsExpected(int n1, int d1, int n2, int d2, string expected)
+        {
+            var f1 = new FractionClass(n1, d1);
+            var f2 = new FractionClass(n2, d2);
+
+            var actual = f1 * f2;
+            Assert.Equal(expected, actual.ToString());
+        }
+
+        [Theory(DisplayName ="Division operator on fractions unit tests.")]
+        [InlineData(1, 5, 3, 7, "7/15")]
+        [InlineData(-2, 3, 1, 4, "-8/3")]
+        [InlineData(1, 5, -4, 9, "-9/20")]
+        [InlineData(1, -5, 4, 9, "-9/20")]
+        [InlineData(1, 5, 4, -9, "-9/20")]
+        [InlineData(0, 5, 4, 9, "0/20")]
+        [InlineData(-1, -5, -4, -9, "9/20")]
+        [InlineData(1, 5, -4, -9, "9/20")]
+        public void DivisionOperator_Fraction_ReturnsExpected(int n1, int d1, int n2, int d2, string expected)
+        {
+            var f1 = new FractionClass(n1, d1);
+            var f2 = new FractionClass(n2, d2);
+
+            var actual = f1 / f2;
+            Assert.Equal(expected, actual.ToString());
+        }
+
+
     }
 }
